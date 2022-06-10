14093e67104458988a713a7cd184480e80fdaf60
Ashish K. Poudel <routeasis@gmail.com>
2022-06-10 07:28:24 +0545

    update handler interface

--- src/interface/feature-filter-handler.interface.ts
+++ src/interface/feature-filter-handler.interface.ts
@@ -1,8 +1,5 @@
 import { IFeatureFilter } from './feature-filter.interface';
 
-export interface IFeatureFilterHandler<
-  TFilter extends IFeatureFilter,
-  TContext extends object = object
-> {
+export interface IFeatureFilterHandler<TFilter extends IFeatureFilter, TContext extends any = any> {
   evaluate(filter: TFilter, context?: TContext): Promise<boolean>;
 }

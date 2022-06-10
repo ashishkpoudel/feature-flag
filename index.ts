745ca3aa7c1ed47ae3291f8fcec592668cd4fb7b
Ashish K. Poudel <routeasis@gmail.com>
2022-06-10 08:46:34 +0545

    update TContext

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 export { FeatureManager } from './src/feature-manager';
 export { IFeature } from './src/interface/feature.interface';
 export { FeatureFlag } from './src/decorator/feature-flag.decorator';
-export { IFeatureFilter } from './src/interface/feature-filter.interface';
-export { IFeatureFilterHandler } from './src/interface/feature-filter-handler.interface';
-export { FeatureFilterHandler } from './src/decorator/feature-filter-handler.decorator';
+export { IStrategy } from './src/interface/strategy.interface';
+export { IStrategyHandler } from './src/interface/strategy-handler.interface';
+export { StrategyHandler } from './src/decorator/strategy-handler.decorator';
 export { IContainer } from './src/interface/container.interface';
 export { containerProvider } from './src/container';
